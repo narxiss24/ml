8d0e5f11ac28dc78b451047901ecd126d42c85b6
Lutfi Fadil Lokman <narxiss@yahoo.com>
2020-11-23 22:17:07 -0500

    Deleted old files

--- Perceptron/iris.py
+++ Perceptron/iris.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %% Get data
+# %% Get data from UCI repository
 
 s = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
 
@@ -10,5 +10,5 @@ df = pd.read_csv(s, header=None, encoding='utf-8')
 
 # %% Select Setosa and Versicolor
 
-y = df.iloc[0:100, 4].values  # Get as an array
+y = df.iloc[0:100, 4].values  # Select first 100 rows, select 4th column, convert to array
 y = np.where(y == 'Iris-setosa', -1, 1)  # label Setosa as 1 and Versicolor as -1
