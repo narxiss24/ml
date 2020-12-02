3964b80b5ed64030e8d2379ca7c1183cb43ce577
Lutfi Fadil Lokman <narxiss@yahoo.com>
2020-12-01 20:04:16 -0500

    new

--- Logistic-Regression/heart_all.py
+++ Logistic-Regression/heart_all.py
@@ -1,68 +1,80 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import statsmodels.api as sm
 from scipy.special import expit
+
 
 # pd.set_option('display.max_rows', None)
 
 def get_data():
     df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data')
-    
-    df.columns = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope', 'ca', 'thal', 'hd']
-    
+
+    df.columns = ['age', 'sex', 'cp', 'trestbps', 'chol', 'fbs', 'restecg', 'thalach', 'exang', 'oldpeak', 'slope',
+                  'ca', 'thal', 'hd']
+
     df['hd'] = df['hd'].apply(lambda x: 1 if x > 0 else x)
-    
+
     # Drop rows that has '?' as missing values
     df = df[~(df == '?').any(axis=1)]
-    
-    df = df[['hd', 'oldpeak', 'sex', 'thalach', 'ca', 'exang']]
-    
-    df = pd.get_dummies(df, columns=['hd', 'sex','exang' ,'ca'], drop_first=True, prefix_sep='')
-    
+
+    df = df[['hd', 'sex', 'oldpeak', 'thalach', 'ca', 'exang']]
+
+    df = pd.get_dummies(df, columns=['hd', 'sex', 'exang', 'ca'], drop_first=True, prefix_sep='')
+
     return df
-    
-    
+
+
 def load_model(df):
     y = df['hd1']
-    
+
     df.drop('hd1', axis=1, inplace=True)
-    
+
     X = sm.add_constant(df)
-    
+
     model = sm.Logit(endog=y, exog=X).fit()
-      
+
     return model
-    
-    
+
+
 def main():
     df = get_data()
-    
+
     model = load_model(df)
-    
+
     print(model.summary())
-    
+
     print(
-    """
-    ====
-    Odds
-    ====
-    """
+        """
+oldpeak: ST depression induced by exercise relative to rest
+thalach: maximum heart rate achieved
+sex: (1 = male; 0 = female)
+ca: number of major vessels (0-3) colored by flourosopy
+exang: exercise induced angina (1 = yes; 0 = no)    
+        """
     )
-    
+
+    print(
+        """
+====
+Odds
+====
+        """
+    )
+
     print(np.exp(model.params))
-    
+
     print(
-    """
-    =============
-    Probabilities
-    =============
-    """
+        """
+=============
+Probabilities
+=============
+        """
     )
-    
-    print(expit(model.params))
-    
+
+    print(expit(model.params[0] + model.params))
+
     return df
-       
+
+
 if __name__ == '__main__':
     main()
-    
--- Logistic-Regression/heart_predict.py
+++ Logistic-Regression/heart_predict.py
@@ -1,5 +1,30 @@
-from heart_all import main
+import heart_all
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
+import matplotlib.pyplot as plt
 
-df = main()
+df = heart_all.get_data()
 
-print(df)
+y = df['hd1']
+
+df.drop('hd1', axis=1, inplace=True)
+
+X = sm.add_constant(df)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = sm.Logit(y_train, X_train).fit()
+
+yhat = model.predict(X_test)
+prediction = list(map(round, yhat))
+
+print('Actual values:', list(y_test.values))
+print('Predictions:', prediction)
+
+print(confusion_matrix(y_test.values, prediction))
+print(accuracy_score(y_test.values, prediction))
+
+plt.plot(yhat, y_test.values, 'bo')
+
+plt.show()
